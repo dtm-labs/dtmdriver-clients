cd7fc1ce9c067b5174ad998281b94c56185cb5df
yedf2 <120050102@qq.com>
2021-12-03 14:32:50 +0800

    gozero example ok

--- gozero/app/main.go
+++ gozero/app/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/yedf/dtmcli/dtmimp"
 	"github.com/yedf/dtmdriver"
 	"github.com/yedf/dtmdriver-clients/busi"
+	"github.com/yedf/dtmdriver-clients/gozero/trans/pb"
 	trans "github.com/yedf/dtmdriver-clients/gozero/trans/pb"
 	_ "github.com/yedf/dtmdriver-gozero"
 	"github.com/yedf/dtmgrpc"
@@ -19,6 +20,8 @@ var (
 	inReq      = &trans.AdjustInfo{Amount: 30, UserID: 2}
 )
 
+var dtmServer = "etcd://localhost:2379/dtmservice"
+
 func main() {
 	flag.Parse()
 
@@ -28,16 +31,33 @@ func main() {
 	err := dtmdriver.Use("dtm-driver-gozero")
 	dtmimp.FatalIfError(err)
 
-	dtmServer := "etcd://localhost:2379/dtmservice"
 	busiServer, err := c.BuildTarget()
 	dtmimp.FatalIfError(err)
 
+	msg(busiServer)
+	tcc(busiServer)
+}
+
+func msg(busiServer string) {
 	gid := dtmgrpc.MustGenGid(dtmServer)
 	msg := dtmgrpc.NewMsgGrpc(dtmServer, gid).
 		Add(busiServer+"/trans.TransSvc/TransOut", &busi.BusiReq{Amount: 30, UserId: 1}).
 		Add(busiServer+"/trans.TransSvc/TransIn", &busi.BusiReq{Amount: 30, UserId: 2})
 
-	err = msg.Submit()
+	err := msg.Submit()
 	dtmimp.FatalIfError(err)
+}
 
+func tcc(busiServer string) {
+	gid := dtmgrpc.MustGenGid(dtmServer)
+	err := dtmgrpc.TccGlobalTransaction(dtmServer, gid, func(tcc *dtmgrpc.TccGrpc) error {
+		rep := pb.Response{}
+		err1 := tcc.CallBranch(&busi.BusiReq{Amount: 30, UserId: 1},
+			busiServer+"/trans.TransSvc/TransOut",
+			busiServer+"/trans.TransSvc/TransOut", // 应当为TransOutConfirm，后续会完善例子
+			busiServer+"/trans.TransSvc/TransOut", // 应当为TransOutCancel，后续会完善例子
+			&rep)
+		return err1
+	})
+	dtmimp.FatalIfError(err)
 }
